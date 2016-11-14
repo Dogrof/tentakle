b3d380173b889c0683c7d1c3206c86a14db32694
Dogrof <Zeron1@bk.ru>
2016-11-14 03:52:29 +0200

    Deleted ConnectDb, trying to Insert values

--- include/RegisterForm.php
+++ include/RegisterForm.php
@@ -5,13 +5,25 @@
  * Date: 12.11.2016
  * Time: 16:55
  */
-require_once 'ConnectDb.php';
+$db_host = 'localhost';
+$db_user = 'tentakle';
+$db_password = '1111';
+$db_name = 'tentakle';
+
+
+$link = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+if (!$link) {
+    die('<p style="color:red">'.mysqli_connect_errno().' - '.mysqli_connect_error().'</p>');
+}
+
 $Email = $_REQUEST['Email'];
 $FName = $_REQUEST['FName'];
 $LName = $_REQUEST['LName'];
 $Pass = $_REQUEST['Pass'];
 $PassConf = $_REQUEST['PassConf'];
-$insert_sql = "INSERT INTO users (email, first_name, last_name)" . "VALUES('{$Email}', '{$FName}', '{$LName}');";
-mysql_query($insert_sql);
-mysql_close($insert_sql);
+$SendInf = "INSERT INTO users (email, first_name, last_name)" . "VALUES('{$Email}', '{$FName}', '{$LName}');";
+
+$Query = mysqli_query($SendInf);
+mysqli_close($link);
 ?>
