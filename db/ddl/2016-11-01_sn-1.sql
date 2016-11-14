73858f9633d5ad1c5e08180a5ae35f48f0e145df
Dogrof <Zeron1@bk.ru>
2016-11-14 20:51:35 +0200

    Cleaning

--- db/ddl/2016-11-01_sn-1.sql
+++
@@ -1,9 +0,0 @@
--- Users
-CREATE TABLE IF NOT EXISTS users (
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	email VARCHAR(40),
-	first_name VARCHAR(20),
-	last_name VARCHAR(20),
-	sex char(1),
-	birth_date DATE
-);
