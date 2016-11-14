4de9776ba76b41522a86c6112dcfc462ed2b1680
Dogrof <Zeron1@bk.ru>
2016-11-14 23:55:02 +0200

    For Stas

---
+++ webapp/db/ddl/2016-11-01_sn-1.sql
@@ -0,0 +1,9 @@
+-- Users
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(40),
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	sex char(1),
+	birth_date DATE
+);
---
+++ webapp/index.php
@@ -0,0 +1,17 @@
+<?php
+echo '<!DOCTYPE HTML>';
+echo '<html>';
+echo '<head>';
+ echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+ echo '<title>Tentakle</title>';
+  echo '</head>';
+  echo '<body background="/source/img/Main.jpg">';
+echo '<h1 align="center"><font color="#7fff00">Добро пожаловать в социальную сеть Тентакль</font></h1>';
+echo '<p align="left"><font color="#ff8c00" size="10"><b>Меню</b></font></p>';
+echo '<ul>';
+ echo '<li><a href="Enter.php"><font color="#ff8c00" size="10">Вход</font></a></li>';
+ echo '<li><a href="Register.php"><font color="#ff8c00" size="10">Регистрация</a></font></li>';
+echo '</ul>';
+  echo '</body>';
+echo '</html>';
+?>
