47eb1fc56cf97e838f9d4d345c48eafc9f027cb5
Dogrof <Zeron1@bk.ru>
2016-11-23 15:56:56 +0200

    Registration page +

---
+++ webapp/db/ddl/2016-11-23_sn-2.sql
@@ -0,0 +1,3 @@
+ALTER TABLE 'users' ADD COLUMN pass CHAR (20)
+ALTER TABLE 'users' DROP `sex`;
+ALTER TABLE 'users' DROP `birth_date`;
---
+++ webapp/source/js/RegisterScripts.js
@@ -0,0 +1,43 @@
+function validateAll(){
+    var Pass = document.getElementById("Pass").value;
+    var PassConf = document.getElementById("PassConf").value;
+    var Email = document.getElementById("Email").value;
+    var Fname = document.getElementById("FName").value;
+    var Lname = document.getElementById("LName").value;
+    var text_fields = document.getElementsByTagName('input');
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    //Проверка всех полей на заполненность
+    for (var i = 0; i < text_fields.length; i++) {
+        if (text_fields[i].type != 'submit') {
+            if (text_fields[i].value == '') {
+                alert("Заполните все поля");
+                return false;
+            }
+        }
+    }
+
+    //Проверка совпадения паролей
+    if (Pass != PassConf) {
+        alert("Вах-Вах-Вах");
+        return false;
+    }
+
+    //Проверка правильности Email
+    if (!re.test(Email)){
+        alert("Неправильный Email");
+        return false;
+    }
+
+    //Проверка правильности имени и фамилии
+    if (Fname.length < 3){
+        alert("Слишком короткое имя");
+        return false;
+    }
+    if (Lname.length < 3){
+        alert("Слишком короткая фамилия");
+        return false;
+    }
+
+    return true;
+}
